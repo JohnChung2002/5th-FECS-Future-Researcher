34f4d7ee222b470db7b6d41b776cdde0af410c88
Noobie000 <just10350r@gmail.com>
2022-08-22 15:54:12 +0800

    Edit database name in script

--- OLAP/mysql-setup.sh
+++ OLAP/mysql-setup.sh
@@ -4,4 +4,4 @@ sudo apt install mysql-server sysbench -y
 sudo service mysql restart
 sudo service mysql start
 sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';"
-sudo mysql -u root -proot -e "CREATE DATABASE sbtest;"
+sudo mysql -u root -proot -e "CREATE DATABASE tpcds;"
