4ee1b1d85bc722de23c656bfdb1e0dae09d9c57d
Noobie000 <just10350r@gmail.com>
2022-08-22 21:28:02 +0800

    Added new query dialect and modified setup file

---
+++ OLAP/queries/Netezza_Postgres/query41.sql
@@ -0,0 +1,51 @@
+-- start query 41 in stream 0 using template query41.tpl
+select  distinct(i_product_name)
+ from item i1
+ where i_manufact_id between 708 and 708+40 
+   and (select count(*) as item_cnt
+        from item
+        where (i_manufact = i1.i_manufact and
+        ((i_category = 'Women' and 
+        (i_color = 'puff' or i_color = 'burlywood') and 
+        (i_units = 'Oz' or i_units = 'Tsp') and
+        (i_size = 'economy' or i_size = 'large')
+        ) or
+        (i_category = 'Women' and
+        (i_color = 'rosy' or i_color = 'misty') and
+        (i_units = 'Cup' or i_units = 'Pallet') and
+        (i_size = 'petite' or i_size = 'medium')
+        ) or
+        (i_category = 'Men' and
+        (i_color = 'maroon' or i_color = 'beige') and
+        (i_units = 'Gross' or i_units = 'Bundle') and
+        (i_size = 'extra large' or i_size = 'small')
+        ) or
+        (i_category = 'Men' and
+        (i_color = 'azure' or i_color = 'coral') and
+        (i_units = 'Ton' or i_units = 'Dram') and
+        (i_size = 'economy' or i_size = 'large')
+        ))) or
+       (i_manufact = i1.i_manufact and
+        ((i_category = 'Women' and 
+        (i_color = 'burnished' or i_color = 'linen') and 
+        (i_units = 'Carton' or i_units = 'Pound') and
+        (i_size = 'economy' or i_size = 'large')
+        ) or
+        (i_category = 'Women' and
+        (i_color = 'lace' or i_color = 'ghost') and
+        (i_units = 'N/A' or i_units = 'Case') and
+        (i_size = 'petite' or i_size = 'medium')
+        ) or
+        (i_category = 'Men' and
+        (i_color = 'purple' or i_color = 'ivory') and
+        (i_units = 'Each' or i_units = 'Tbl') and
+        (i_size = 'extra large' or i_size = 'small')
+        ) or
+        (i_category = 'Men' and
+        (i_color = 'white' or i_color = 'navajo') and
+        (i_units = 'Ounce' or i_units = 'Dozen') and
+        (i_size = 'economy' or i_size = 'large')
+        )))) > 0
+ order by i_product_name
+ limit 100;
+-- end query 41 in stream 0 using template query41.tpl
--- OLTP/tidb-setup.sh
+++ OLTP/tidb-setup.sh
@@ -1,8 +1,13 @@
+#!/bin/bash
 sudo apt update
 sudo apt upgrade -y
 sudo apt install mysql-server sysbench -y
 sudo service mysql stop
 curl --proto '=https' --tlsv1.2 -sSf https://tiup-mirrors.pingcap.com/install.sh | sh
 tiup playground v6.1.0 --db 1 --pd 1 --kv 1 --tiflash 1 --without-monitor &
+until tiup status | grep -q "RUNNING";
+do 
+  sleep 1; 
+done
 sudo mysql --host 127.0.0.1 --port 4000 -u root -p -e "SET PASSWORD='root';"
 sudo mysql --host 127.0.0.1 --port 4000 -u root -proot -e "CREATE DATABASE sbtest;"
