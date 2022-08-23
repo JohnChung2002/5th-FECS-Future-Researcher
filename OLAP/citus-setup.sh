6313df82b969eae2e032470ac048edf19fc36764
Noobie000 <just10350r@gmail.com>
2022-08-24 01:55:29 +0800

    update dependencies

--- OLAP/citus-setup.sh
+++ OLAP/citus-setup.sh
@@ -1,7 +1,7 @@
 sudo apt update
 sudo apt upgrade -y
 curl https://install.citusdata.com/community/deb.sh | sudo bash
-sudo apt-get -y install postgresql-14-citus-11.0 sysbench
+sudo apt-get -y install postgresql-14-citus-11.0 libpq-dev
 cp postgresql.conf.sample /usr/share/postgresql/14/postgresql.conf.sample
 sudo cp citus-addon.sh /var/lib/postgresql
 sudo su -c "./citus-addon.sh" postgres
--- OLAP/postgresql-setup.sh
+++ OLAP/postgresql-setup.sh
@@ -3,7 +3,7 @@ sudo apt upgrade -y
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt update
-sudo apt -y install postgresql-14
+sudo apt -y install postgresql-14 libpq-dev
 cp postgresql.conf.sample /usr/share/postgresql/14/postgresql.conf.sample
 pg_createcluster 14 main --start
 sudo -u postgres psql -c "ALTER USER postgres with password 'postgres';"
